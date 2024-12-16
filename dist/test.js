988ed5f19e37619e1d9d9015975301a286fbbc97
dor <doratzabi1@gmail.com>
2024-12-16 09:53:07 +0200

    fix

--- dist/test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./my-database.db');
-db.all('SELECT * FROM Product WHERE ProductName = "Metadefender Vault" AND VendorId = "1" ', function (err, rows) {
-    console.log(rows);
-});
