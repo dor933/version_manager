ee055b89cb9f30cb619644d79c09dec131681f82
dor <doratzabi1@gmail.com>
2025-01-21 17:51:13 +0200

    comit

--- client/src/Notification.tsx
+++ client/src/Notification.tsx
@@ -19,7 +19,7 @@ const emailSchema = z.string().email();
 
 const customTextFieldStyle = {
   '& .MuiOutlinedInput-root': {
-    height: '50px',
+    height: '53.5px',
     fontFamily: 'Kumbh Sans',
     '& fieldset': {
       borderColor: '#E0E0E0',
