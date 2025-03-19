d5e241b6f0bddee06213af511e0b3302d7a4972d
Dor Ratzabi <dorr@bulwarx.local>
2025-03-19 09:45:48 +0200

    comit

--- server/src/api/routes/Versions.ts
+++ server/src/api/routes/Versions.ts
@@ -1,32 +1,28 @@
-import express from 'express';
-import { db, logger } from '../../BackendLogic/index';
-import { getproductsandmodules } from '../../BackendLogic/Functions/LogicFunctions';
+import express from "express";
+import { db, logger } from "../../BackendLogic/index";
+import { getproductsandmodules } from "../../BackendLogic/Functions/LogicFunctions";
 
 const router = express.Router();
 
-
-router.get('/versions', async (req, res) => {
-    try{
+router.get("/versions", async (_, res) => {
+  try {
     const versions = await db.getVersions();
-   
-    let products:any= await db.getProducts();
-  
- 
-    let productsandmodules= await getproductsandmodules(products);
-    
-    res.json({versions, productsandmodules});
-    }
-    catch(error){
-        logger.error('Error in getVersions', error);
-        throw error;
-    }
+
+    let products: any = await db.getProducts();
+
+    let productsandmodules = await getproductsandmodules(products);
+
+    res.json({ versions, productsandmodules });
+  } catch (error) {
+    logger.error("Error in getVersions", error);
+    throw error;
+  }
 });
 
- router.get('/sync', async (req, res) => {
-     const sync = await db.HandleData();
-     const versions = await db.getVersions();
-     res.json({sync, versions});
- });
- 
+router.get("/sync", async (_, res) => {
+  const sync = await db.HandleData();
+  const versions = await db.getVersions();
+  res.json({ sync, versions });
+});
 
-export default router; 
+export default router;
